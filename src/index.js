0ff1aa194267ef9ebcc31bb1b0ea962f73258a89
jetrotullroa <jetrotullroa@gmail.com>
2016-04-10 14:56:15 +0800

    Revert "refactor App function to a class and create a new function inside to pullout APIKEY and connect to youtube-api"
    This reverts commit 788b02fb0dd01b0905b1ecf26e424e7048d43c5a.

--- src/index.js
+++ src/index.js
@@ -1,33 +1,19 @@
 // import from dependencies
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import YTSearch from 'youtube-api-search';
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+//import from cmponent files
 import SearchBar from './components/search_bar.js';
 
 // Youtube API
 const API_KEY = 'AIzaSyAkMb7LUi72EtkKxhOtkrglTCmXOh-hIQw';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
 
-    this.state = { videos: [] };
+// COMPONENTS FOR THE DOM
+const App = () => {
+  return <SearchBar />
+};
 
-    YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
-      this.setState({ videos });
-    });
-
-  }
-
-
-  render() {
-    return (
-      <div>
-        <SearchBar />
-      </div>
-    );
-  }
-}
 
 
 // RENDER COMPONENTS TO DOM
